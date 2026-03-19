d009fa4ac8cf95644bc10d2d1dbfe74969c2d796
Marc Zander <m4rc2a.opensource@tutanota.de>
2026-03-19 15:30:32 +0000

    chore: configure repo for EIS wiki

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,20 +8,18 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "🧊 Wiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    analytics: null,
+    locale: "de-DE",
+    baseUrl: "m4rc2a.github.io/eis-wiki/",
+    ignorePatterns: ["private", "templates", "textgenerator", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
-      cdnCaching: true,
+      cdnCaching: false,
       typography: {
         header: "Schibsted Grotesk",
         body: "Source Sans Pro",
